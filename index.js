19d6f727d9872b6b18c5b8b504848dbb0e606e1c
srph <seraphipod@gmail.com>
2015-10-19 16:54:07 +0800

    UMDify haha

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 var axios = require('axios');
 
-module.exports = axios.interceptors.response.use(
-  function logResponse(res) {
-    console.log('%c Request Success:', 'color: #4CAF50; font-weight: bold', res);
-    return res;
-  },
-
-  function logPromiseError(err) {
-    console.log('%c Request Error:', 'color: #EC6060; font-weight: bold', err);
-    return Promise.reject(err);
+function (root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    // AMD. Register as an anonymous module.
+    define(['axios'], factory);
+  } else if (typeof module === 'object' && module.exports) {
+    // CommonJS
+    factory(require('axios'));
+  } else {
+    // Browser globals
+    factory(root.axios);
   }
-);
+}(this, function (axios) {
+  return axios.interceptors.response.use(
+    function logResponse(res) {
+      console.log('%c Request Success:', 'color: #4CAF50; font-weight: bold', res);
+      return res;
+    },
+
+    function logPromiseError(err) {
+      console.log('%c Request Error:', 'color: #EC6060; font-weight: bold', err);
+      return Promise.reject(err);
+    }
+  );
+}));
