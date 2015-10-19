b6abd5a1fa6df0a99c4ee45cb3ca8b07ef603a70
srph <seraphipod@gmail.com>
2015-10-19 17:34:31 +0800

    Fix missing `(`.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var axios = require('axios');
 
-function (root, factory) {
+(function (root, factory) {
   if (typeof define === 'function' && define.amd) {
     // AMD. Register as an anonymous module.
     define(['axios'], factory);
